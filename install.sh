bbf5bc02996b9699eccd34f9a36c373e632403ea
ETJAKEOC <etjakeoc@gmail.com>
2025-08-19 11:16:29 -0400

    Finally fixing the time make env for good?

--- install.sh
+++ install.sh
@@ -253,11 +253,11 @@ _gen_kern_name() {
     if [[ "$_modprobeddb" == "true" || "$_kernel_on_diet" == "true" ]]; then
       msg2 "Building modprobed/diet kernel..."
       # The 'localmodconfig' target is added for diet builds.
-      time ${compiler_opt} make ${verbose_opt} LSMOD="$_modprobeddb_db_path" localmodconfig bzImage modules -j"${_thread_num}" "$@"
+      "${compiler_opt}" time (make "${verbose_opt}" LSMOD="$_modprobeddb_db_path" localmodconfig bzImage modules -j"${_thread_num}" "$@")
     else
       msg2 "Building kernel..."
       # Generic build does not need the extra target or LSMOD variable.
-      time ${compiler_opt} make ${verbose_opt} bzImage modules -j"${_thread_num}" "$@"
+      "${compiler_opt}" time (make "${verbose_opt}" bzImage modules -j"${_thread_num}" "$@")
     fi
 
   }
